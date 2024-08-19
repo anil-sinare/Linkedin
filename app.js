56b9df0b1093ec58dc11396f2f23b4fd192e13f5
anilsinare2003@gmail.com <145422873+anil-sinare@users.noreply.github.com>
2024-08-19 11:09:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+var profilemenuwrap = document.querySelector(".profile-menu-wrap")
+
+function settingmenutoggle(){
+    profilemenuwrap.classList.toggle("setting-menu-height")
+}
+
+
+
+let sideactivity = document.getElementById("sidebaractivity");
+let morelink = document.getElementById("showmorelink");
+
+function toggleActivity(){
+    sideactivity.classList.toggle("open-activity");
+}
